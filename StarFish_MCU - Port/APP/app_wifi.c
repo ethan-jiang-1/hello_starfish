8612e3e7a03ac02bcaa38a0b57946fc59789ef57
zhang-gang <zgshou@hotmail.com>
2014-12-10 16:53:19 +0800

    Implement function table to handle message ID
    1. Clean code
    2. Add function table to handle common message

--- StarFish_MCU - Port/APP/app_wifi.c
+++ StarFish_MCU - Port/APP/app_wifi.c
@@ -17,49 +17,29 @@ static uint_32  g_uart0_rx_queue[sizeof(LWMSGQ_STRUCT)/sizeof(uint_32) + WIFI_MS
 #define WIFI_RECV_BUF_SIZE     512  /*This is maxium buffer size*/
 static uint_8  g_wifi_com_recv_buf[WIFI_RECV_BUF_SIZE];
 
-static es201_dev_info_t         g_es201_gps_dev;
 
-LWSEM_STRUCT                    g_mma8451_alarm_sem;
-static LWSEM_STRUCT             g_at_cmd_send_sem;
-
-
-
-///---------------------------------------------
-#define GPS_NO_SIGNAL_CMD       "+GPSINFO: GPS NO SINGAL"
-#define GPS_HEADER_CMD          "+GPSINFO: "
-#define ES201_GPS_POWER_STATE   "+GPS: "
-#define ES201_POWER_ON_CMD      "IIII"
-#define ES201_OK_AT_CMD         "OK"
-#define ES201_ERROR_AT_CMD      "ERROR"
-
-#define ES201_AT_TEST_CMD       "AT"
-#define ES201_GPS_INFO_AT_CMD   "AT+GPSINFO?"
-#define OPEN_GPS_POWER          "AT+GPS=1"
-#define GET_GPS_POWER_STATE     "AT+GPS?"
-#define ES201_AT_CSQ_CMD        "AT+CSQ"
 //////////////////////WIFI CMD
-#define WIFI_TAG_HEADER				"%B1Q"
+#define WIFI_TAG_HEADER				"%B1Q,"
 #define WIFI_IMAGE_TAG_HEADER							"%B1Q,300,"	
 #define WIFI_IMAGE_TAG_LEN								(sizeof(WIFI_IMAGE_TAG_HEADER) - 1)
 #define WIFI_IMAGE_CMD_HEADER_LEN						(sizeof("%B1Q,300,1000,1,0123456789"))
 
-///---------------------------------------------
-// sms send
-#define ES201_AT_CIMI           "AT+CIMI"
-#define ES201_AT_CSMS           "AT+CSMS=1"
-#define ES201_AT_CNMI           "AT+CNMI=2,2"
-#define ES201_AT_CMGF           "AT+CMGF=1"
-#define ES201_AT_SEND_SMS       "AT+CMGS="
-#define ES201_SMS_SEND_IND      ">"
-// sms receive
-#define ES201_RECV_NEW_MSG      "+CMT: \""
-
-// lock phone number
-#define LOCK_PHONE_NUMBER       "13616212725"
 
 
 static void uart0_irq_handler(void* p_arg);
-static void send_at_command(char* p_cmd);
+void test1()
+{
+	printf("test1\r\n");
+}
+void test2()
+{
+	printf("test2\r\n");
+}
+
+void test3()
+{
+	printf("test3\r\n");
+}
 
 bool verifyCheckSum( dword dwTargetCheckSum, uint_8 *pData, dword nDataLen )
 {
@@ -128,173 +108,36 @@ static int  do_wifi_image_cmd(uint_8* pData)
 	return 1;
 	
 }
-static int  do_wifi_xxx_cmd(char* p_str)
-{
-    char* delims = ",";
-    char* p_result;
-    es201_gps_handle v;
-    int   i,length, lastframe= 0;
 
-    // first, get es201 device instance
-    v = get_es201_gps_instance();
 
-    // 1. length
-    p_result = strtok(p_str, (char*)delims);
-    if (p_result)
-    {
-        length = atoi(p_result);
-    }
-    else
-    {
-        return -1;
-    }
-
-    // 2. last frame flag
-    p_result = strtok( NULL, (char*)delims);
-    if (p_result)
-    {
-        lastframe = atoi(p_result);
-    }
-    else
-    {
-			
-        return -2;
-    }
-
-    // 3. altitude
-    p_result = strtok( NULL, (char*)delims);
-    if (p_result)
-    {
-        v->altitude = atof(p_result);
-    }
-    else
-    {
-        v->gps_signal = GPS_POWER_ON;
-        return -3;
-    }
-
-    // 4. satellite number, no care about it
-    p_result = strtok( NULL, (char*)delims);
-    if (p_result)
-    {
-        v->satellite_num = atoi(p_result);
-    }
-    else
-    {
-        v->gps_signal = GPS_POWER_ON;
-        return -4;
-    }
-
-    // 5. speed
-    p_result = strtok( NULL, (char*)delims);
-    if (p_result)
-    {
-        v->speed = atof(p_result);
-    }
-    else
-    {
-        v->gps_signal = GPS_POWER_ON;
-        return -5;
-    }
-
-    // 6. no care about it
-    p_result = strtok( NULL, (char*)delims);
-    if (p_result)
-    {
-    }
-    else
-    {
-        v->gps_signal = GPS_POWER_ON;
-        return -6;
-    }
-
-    // 7. UTC time
-    p_result = strtok( NULL, (char*)delims);
-    if (p_result)
-    {
-        snprintf(v->utc_time, sizeof(v->utc_time), "%s", p_result);
-    }
-    else
-    {
-        v->gps_signal = GPS_POWER_ON;
-        return -7;
-    }
-
-    // 8. GPS fix information
-    p_result = strtok(NULL, (char*)delims);
-    if (p_result)
-    {
-        i = atoi(p_result) + GPS_POWER_OFF;
-        if (IS_GPS_STATE_TYPE(i))
-        {
-            v->gps_signal = (gps_state)i;
-        }
-        else
-        {
-            v->gps_signal = GPS_POWER_ON;
-            return -8;
-        }
-    }
-    else
-    {
-        v->gps_signal = GPS_POWER_ON;
-        return -9;
-    }
-
-    return 0;
-
-}
-
-/*
- * @brief: deal with a new sms message
- *
- * +CMT: "+8613616212725","","14/03/17,20:53:58+32"
- * ggggggggggg
- */
-static int  do_sms_cmd(char* p_str)
-{
-    char* delims = "\"\r\n";
-    char* p_result;
-    char  phone_number_buffer[16];
-
-    p_result = p_str + strlen(ES201_RECV_NEW_MSG);
-    memcpy(phone_number_buffer, p_result, 14);
-    phone_number_buffer[14] = 0;
-
-    APP_TRACE("phone number is: %s\r\n", phone_number_buffer);
-
-    // 1. Don't care about the front part of the sms message
-    p_result = strtok(p_str, (char*)delims);
-    if (!p_result)
-    {
-        APP_TRACE("The sms message is wrong!\r\n");
-        DUMP_DATA((uint8_t*)p_str, strlen(p_str));
-    }
-
-    // 2. the sms message
-    p_result = strtok(NULL, (char*)delims);
-    if (p_result)
-    {
-        APP_TRACE("The sms message: %s\r\n", p_result);
-    }
-
-    return 0;
-}
-
-int handleMessage(char* pData)
+int HandleMessage(char* pData)
 {
 	 char* delims = ",";
-    int id,i= 0;
+    int id,nSize,i= 0;
 		int para[5]={0};
 		char *p_result;
 		char *buf= pData;
+		FComFunPair funTbl [] = {FCOM_FUNC_TBL};
+ printf("%s\r\n",(const char*)buf);
 		while((p_result=strtok(buf,(char*)delims))!=NULL) {
-                para[i]=atoi(p_result); 
+			
+                para[i]=atoi(p_result);
 								i++;
                  buf=NULL;
 				}
-				printf("%d,%d,%d\r\n",para[0],para[1],para[2]);
-    id=para[0];
+				printf("s1:%d,s2:%d,s3:%d\r\n",para[0],para[1],para[2]);
+
+	id=para[0];
+ nSize = sizeof(funTbl) / sizeof(funTbl[0]);			
+	for(i=0; i<nSize; ++i)
+	{
+		
+		if(funTbl[i].m_nID==id)
+		{
+			(*funTbl[i].m_pFunAddr)();
+		}
+			
+	}		
 		switch(id){
 			case 100:
 					 
@@ -319,15 +162,12 @@ bool IsImage()
   * @param: length, reception character length
   * @retval: none
   * @note:
-  * AT command example:
-  * 1) GPS data packetage information
   *
   */
 void    do_cmd_wifidata(uint8_t length)
 {
     char*               p;
     int                 ret;
-    es201_gps_handle    v;
     int                 state;
 
     // first, in order to deal with the string
@@ -352,327 +192,15 @@ void    do_cmd_wifidata(uint8_t length)
 		 if(p)
 		 {
 			  p += strlen(WIFI_TAG_HEADER);
-			 ret=handleMessage(p);
+			 ret=HandleMessage(p);
 		 }
-		 /////////////////////////////
 		
-//		if(p)
-//		{
-//			 // skip to GPS data content
-//       // p += strlen(GPS_HEADER_CMD);
-//        ret = do_wifi_image_cmd(p);
-//        if (ret == 0)
-//        {
-//            APP_TRACE("%.5f, %.5f, %.5f, %.5f, %d, %s, %d\r\n",
-//                      v->longitude,
-//                      v->latitude,
-//                      v->altitude,
-//                      v->speed,
-//                      v->satellite_num,
-//                      v->utc_time,
-//                      (int)v->gps_signal);
-//        }
-//        else
-//        {
-//            APP_TRACE("get GPS signal error[%d]\r\n", ret);
-//            APP_TRACE("%s", g_wifi_com_recv_buf);
-//        }
-
-//        return;
-//		}
-//	
-//	//end of wifi parsing
-//    v = get_es201_gps_instance();
-
-//    // GPS no signal
-//    // +GPSINFO: GPS NO SINGAL
-//    p = strstr((const char*)g_wifi_com_recv_buf, GPS_NO_SIGNAL_CMD);
-//    if (p)
-//    {
-//        v->gps_signal = GPS_POWER_ON;
-//        return;
-//    }
-
-
-//    // send sms ind
-//    p = strstr((const char*)g_wifi_com_recv_buf, ES201_SMS_SEND_IND);
-//    if (p)
-//    {
-//        _lwsem_post(&g_at_cmd_send_sem);
-//        return;
-//    }
-
-//    /*
-//     * @brief: receive a new sms message
-//     *
-//     * +CMT: "+8613616212725","","14/03/17,20:53:58+32"
-//     * ggggggggggg
-//     */
-//    p = strstr((const char*)g_wifi_com_recv_buf, ES201_RECV_NEW_MSG);
-//    if (p)
-//    {
-//        do_sms_cmd((char*)g_wifi_com_recv_buf);
-//    }
-
-//    // ES201 power on information
-//    // IIII....
-//    p = strstr((const char*)g_wifi_com_recv_buf, ES201_POWER_ON_CMD);
-//    if (p)
-//    {
-//        v->gps_signal = GPS_POWER_OFF;
-//        v->gsm_state  = GSM_POWER_ON;
-
-//        return;
-//    }
-
-//    // ES201 power state information
-//    // +GPS: 1      ---> GPS is power on
-//    // +GPS: 0      ---> GPS is power off
-//    p = strstr((const char*)g_wifi_com_recv_buf, ES201_GPS_POWER_STATE);
-//    if (p)
-//    {
-//        p += strlen(ES201_GPS_POWER_STATE);
-//        state = atoi(p);
-//        if (state == 1)
-//            v->gps_signal = GPS_POWER_ON;
-//        else
-//            v->gps_signal = GPS_POWER_OFF;
-
-//        return;
-//    }
-
-//    // normal AT cmd information
-//    // OK
-//    p = strstr((const char*)g_wifi_com_recv_buf, ES201_OK_AT_CMD);
-//    if (p)
-//    {
-//        v->gsm_state = GSM_STATE_OK;
-//        return;
-//    }
-
-//    // ERROR
-//    p = strstr((const char*)g_wifi_com_recv_buf, ES201_ERROR_AT_CMD);
-//    if (p)
-//    {
-//        v->gsm_state = GSM_STATE_ERROR;
-//        return;
-//    }
-
-//    APP_TRACE("\r\n* ES201 data not handle:\r\n");
-//    DUMP_DATA(g_wifi_com_recv_buf, length);
 
 }
 
 
-es201_gps_handle get_es201_gps_instance(void)
-{
-    return &g_es201_gps_dev;
-}
 
 
-void    send_sms_alarm_command(char* p_phone)
-{
-    _mqx_uint           ret;
-    char                vl_buf[64];
-    es201_gps_handle    v;
-    uint8_t             i;
-
-    v = get_es201_gps_instance();
-
-    for (i = 0; i < 5; i++)
-    {
-        send_at_command("AT");
-        _time_delay_ticks(200);
-    }
-
-    snprintf(vl_buf, sizeof vl_buf, "AT+CMGS=\"%s\"", p_phone);
-    send_at_command(vl_buf);
-
-    // wait '>'
-    ret = _lwsem_wait_ticks(&g_at_cmd_send_sem, 200*3);
-    if (ret != MQX_OK)
-    {
-        APP_TRACE("SMS wait '>' error!\r\n");
-//        set_rgb_led_state(RGB_GREEN_LED | RGB_RED_LED);
-    }
-    else
-    {
-        snprintf(vl_buf, sizeof vl_buf, "!!!alarm: %.5f,%.5f,%.2f",
-                                        v->longitude,
-                                        v->latitude,
-                                        v->speed);
-
-        // OK, send the alarm message
-        uart0_send_string((uint8_t*)vl_buf);
-        // CTRL-Z
-        uart0_send_data(0x1A);
-    }
-
-}
-
-
-void    send_sms_alarm_message(void)
-{
-    es201_gps_handle        v;
-    _mqx_uint               current_ticks;
-    KERNEL_DATA_STRUCT_PTR  kernel_data;
-
-    v = get_es201_gps_instance();
-
-    _GET_KERNEL_DATA(kernel_data);
-
-    current_ticks = kernel_data->TIME.TICKS[0];
-    if (current_ticks - v->ticks >= 200*60*3)   // 3min
-    {
-        v->alarm = 0;
-        v->ticks = current_ticks;
-    }
-
-    if (v->alarm == 0)
-    {
-        v->alarm = 1;
-        // TODO : phone number need to be saved in flash
-        send_sms_alarm_command("13616212725");
-        _time_delay_ticks(200);
-    }
-
-}
-
-void    get_gps_information(void)
-{
-    es201_gps_handle    v;
-
-    v = get_es201_gps_instance();
-
-    // update gps information
-    if (v->gps_signal == GPS_POWER_ON || v->alarm)
-    {
-        send_at_command(ES201_GPS_INFO_AT_CMD);
-    }
-
-}
-
-static void send_at_command(char* p_cmd)
-{
-    uart0_send_string((uint8*)p_cmd);
-    uart0_send_data(0x0D);
-}
-
-
-/**
-  * @brief: sending AT command to communication with ES201
-  * 1. ES201 power off : blue led
-  * 2. ES201 power on  : red led
-  * 3. ES201 GPS signal: green led
-  *
-  */
-void    app_es201_send_task(uint32_t task_init_data)
-{
-    _mqx_uint           ret;
-    es201_gps_handle    v;
-    int                 i;
-
-    APP_TRACE("app_es201_send_task...\r\n");
-
-   // set_rgb_led_state(RGB_BLUE_LED);
-    v = get_es201_gps_instance();
-
-    // delay 5 seconds...
-    _time_delay_ticks(200*5);
-
-    // send AT command to test with ES201
-    for (i = 0; i < 5; i++)
-    {
-        // AT
-        send_at_command(ES201_AT_TEST_CMD);
-        _time_delay_ticks(200);
-    }
-
-    // get GSM network signal
-    send_at_command(ES201_AT_CSQ_CMD);
-    _time_delay_ticks(200);
-
-    if (v->gsm_state < GSM_POWER_ON)
-    {
-        APP_TRACE("ES201 no response, please power on...\r\n");
-
-        // wait 30 seconds
-        for (i = 0; i < 30; i++)
-        {
-            _time_delay_ticks(200);
-            if (v->gsm_state >= GSM_POWER_ON)
-            {
-                APP_TRACE("ES201 POWER ON!\r\n");
-
-                // set RGB to purple
-            //    set_rgb_led_state(RGB_RED_LED | RGB_BLUE_LED);
-                // delay 7 seconds to wait ES201 ready
-                _time_delay_ticks(200*7);
-                break;
-            }
-        }
-
-        if (i == 20)
-        {
-            // system error, send task return
-            APP_TRACE("!!! ES201 POWER ON ERROR !!!\r\n");
-            return;
-        }
-    }
-    else
-    {
-        APP_TRACE("ES201 IS POWER ON ALREADY!\r\n");
-    }
-
-    // -------------------- set SMS direct show --------------------
-    _time_delay_ticks(200);
-    send_at_command(ES201_AT_CIMI);
-
-    _time_delay_ticks(200);
-    send_at_command(ES201_AT_CNMI);
-
-    _time_delay_ticks(200);
-    send_at_command(ES201_AT_CMGF);
-    _time_delay_ticks(200);
-
-    // ---------------------------- GPS ----------------------------
- //   set_rgb_led_state(RGB_RED_LED);
-
-    // check GPS state
-    for (i = 0; i < 5; i++)
-    {
-        if (v->gps_signal >= GPS_POWER_ON)
-        {
-            break;
-        }
-
-        send_at_command(GET_GPS_POWER_STATE);
-        _time_delay_ticks(200);
-    }
-
-    if (i == 5)
-    {
-        APP_TRACE("open GPS power...\r\n");
-        send_at_command(OPEN_GPS_POWER);
-        _time_delay_ticks(200);
-    }
-
-    APP_TRACE("ES201 is ready to monitoring\r\n");
-    for (;;)
-    {
-        ret = _lwsem_wait_ticks(&g_mma8451_alarm_sem, 200*3);
-        switch (ret)
-        {
-            case MQX_OK:                    // send sms
-                send_sms_alarm_message();
-                break;
-            case MQX_LWSEM_WAIT_TIMEOUT:    // check GPS signal
-                get_gps_information();
-                break;
-        }
-    }
-
-}
 
 
 void    app_wifi_control_task(uint32_t task_init_data)
@@ -680,32 +208,15 @@ void    app_wifi_control_task(uint32_t task_init_data)
     _mqx_uint           ret;
     _mqx_max_type       msg = 0;
     uint8_t             pos = 0;
-    es201_gps_handle    v;
-    _task_id            id;
 
     // create recv msg queue
     ret = _lwmsgq_init(g_uart0_rx_queue, WIFI_MSG_NUMBER, WIFI_MSG_SIZE);
     ASSERT_PARAM(MQX_OK == ret);
 
-//    // create send AT command sem
-//    ret = _lwsem_create(&g_at_cmd_send_sem, 0);
-//    ASSERT_PARAM(MQX_OK == ret);
 
-//    ret = _lwsem_create(&g_mma8451_alarm_sem, 0);
-//    ASSERT_PARAM(MQX_OK == ret);
-
-    // init UART0 to communication with ES201
+    // init UART0 to communication with wifi
     init_uart0(uart0_irq_handler);
 
-    // clear es201 device instance
-    v = get_es201_gps_instance();
-    memset(v, 0x00, sizeof(es201_dev_info_t));
-
-    // start ES201 at command send task
-   // id = _task_create_at(0, ES201_SEND_TASK, 0, es201_send_task_stack, ES201_SEND_TASK_STACK_SIZE);
-    ASSERT_PARAM(MQX_NULL_TASK_ID != id);
-
-    APP_TRACE("app_es201_control_task start...\r\n");
 
     for (;;)
     {
--- StarFish_MCU - Port/APP/app_wifi.h
+++ StarFish_MCU - Port/APP/app_wifi.h
@@ -21,63 +21,18 @@ typedef struct
 		uint_32	m_lEndFlag;
 		uint_32	m_dwCheckSum;
 	} wifi_image_info;
-typedef enum
-{
-    GPS_POWER_OFF,          /* 0 GPS in power off(default)  */
-    GPS_POWER_ON,           /* 1 GPS is on, but no signal   */
-    GPS_2D_FIX,             /* 2 2D fix                     */
-    GPS_3D_FIX              /* 3 3D fix                     */
-}gps_state;
 
-#define IS_GPS_STATE_TYPE(TYPE)    (((TYPE) == GPS_POWER_OFF) || \
-                                    ((TYPE) == GPS_POWER_ON)  || \
-                                    ((TYPE) == GPS_2D_FIX)  || \
-                                    ((TYPE) == GPS_3D_FIX))
+typedef void (*HandleFun) ();
+typedef struct 
+	{
+		int	m_nID;
+		HandleFun		m_pFunAddr;
+	}FComFunPair;	
 
-typedef enum
-{
-    GSM_POWER_OFF,          /* 0 ES201 power off(default)   */
-    GSM_POWER_ON,           /* 1 ES201 power on             */
-    GSM_SENDING_AT_CMD,     /* 2 sending AT command         */
-    GSM_STATE_IDLE,         /* 3 ES201 in idle state        */
-    GSM_STATE_OK,           /* 4 OK                         */
-    GSM_STATE_ERROR         /* 5 ERROR                      */
-}gsm_state;
-
-
-typedef struct
-{
-    // GPS related information
-    gps_state   gps_signal;         // GPS signal state
-    float       longitude;          // longitude, 122.07391
-    float       latitude;           // latitude, 37.52375
-    float       altitude;           // altitude, 53.87
-    float       speed;              // device speed
-    int         satellite_num;      // satellite number
-    char        utc_time[16];       // 140215220304
-
-    // GSM state
-    gsm_state   gsm_state;
-
-    // alarm state
-    char        alarm;
-
-    // ticks
-    uint_64     ticks;
-}es201_dev_info_t;
-
-
-typedef es201_dev_info_t *es201_gps_handle;
-
-
-extern LWSEM_STRUCT             g_mma8451_alarm_sem;
-
-
-
+	#define FCOM_FUNC_TBL		{100,				&test1},\
+                        {101,         &test2},\
+												{102,			&test3}
 void    app_wifi_control_task(uint32_t task_init_data);
-void    app_es201_send_task(uint32_t task_init_data);
-
-es201_gps_handle get_es201_gps_instance(void);
 
 
 
